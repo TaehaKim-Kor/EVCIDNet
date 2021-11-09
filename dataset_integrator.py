3a879b14c6a045be8fc18d29ef7ccf6a9e450f2c
TaehaKim <amvision1248@gmail.com>
2021-11-09 17:53:29 +0900

    Fixed the variables' position to data_preprocess/myval.py

--- dataset_integrator.py
+++ dataset_integrator.py
@@ -2,7 +2,11 @@ import json
 from glob import glob
 import shutil
 import os
-from dataset_preprocess.dataset import dataset_maker
+from dataset_preprocess.dataset import dataset_maker, coco_json_integrator
 from dataset_preprocess.myval import *
 
+#Prototype Integration function
+#coco_json_integrator(data_type, file_type, input_path)
+
+#Main function
 dataset_maker(data_type, file_type, input_path)
