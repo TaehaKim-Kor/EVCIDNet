c0b33f05c9ae98fe6c7c54fdb4da0f2e4b6e6dfc
TaehaKim <amvision1248@gmail.com>
2021-11-09 17:51:06 +0900

    Fixed the variables' position to data_preprocess/myval.py

--- dataset_integrator.py
+++ dataset_integrator.py
@@ -3,12 +3,6 @@ from glob import glob
 import shutil
 import os
 from dataset_preprocess.dataset import dataset_maker
+from dataset_preprocess.myval import *
 
-data_type = "train"
-file_type = ".json"
-input_path = "C:\\Users\\User\\Desktop\\annotation_test\\"
-output_path = "E:\\annotation_integrated\\"
-#data_type = "instances_default"
-#file_type=".json"
-#input_path="E:/EVlegacy/"
 dataset_maker(data_type, file_type, input_path)
--- dataset_preprocess/myval.py
+++ dataset_preprocess/myval.py
@@ -1,6 +1,12 @@
+data_type = "train"
+file_type = ".json"
+input_path = "C:\\Users\\User\\Desktop\\annotation_test\\"
+output_path = "E:\\annotation_integrated\\"
+
 json_dict = {"licenses": [{"name": "", "id": 0, "url": ""}],
              "info": {"contributor": "", "date_created": "", "description": "", "url": "", "version": "", "year": ""},
              "categories": [{"id": 1, "name": "HolePairLeft", "supercategory": ""},
                             {"id": 2, "name": "HolePairRight", "supercategory": ""},
                             {"id": 3, "name": "ACHole", "supercategory": ""}],
              "images": {}, "annotations": {}}
+
