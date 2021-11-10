4e75b395773f716e3a5c32744c1950e69e27e371
TaehaKim <amvision1248@gmail.com>
2021-11-10 14:23:33 +0900

    Update code. Variable modified.

--- dataset_integrator.py
+++ dataset_integrator.py
@@ -9,4 +9,4 @@ from dataset_preprocess.myval import *
 #coco_json_integrator(data_type, file_type, input_path)
 
 #Main Fucntion for Dataset Integration
-dataset_maker(data_type, file_type, input_path)
+dataset_maker(input_path, output_folder, researcher, data_type, file_type)
