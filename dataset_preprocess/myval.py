c3ab1fc950b554c8546503f239f5e4f5b272bc73
TaehaKim <amvision1248@gmail.com>
2021-11-10 14:35:13 +0900

    json_dict type error modified.

--- dataset_preprocess/myval.py
+++ dataset_preprocess/myval.py
@@ -8,5 +8,5 @@ json_dict = {"licenses": [{"name": "", "id": 0, "url": ""}],
              "categories": [{"id": 1, "name": "HolePairLeft", "supercategory": ""},
                             {"id": 2, "name": "HolePairRight", "supercategory": ""},
                             {"id": 3, "name": "ACHole", "supercategory": ""}],
-             "images": {}, "annotations": {}}
+             "images": [], "annotations": []}
 
