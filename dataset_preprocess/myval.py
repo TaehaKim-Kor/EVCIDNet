e0b4a7f8359f74fb70428f7eafb0a5dc3143db3a
TaehaKim <amvision1248@gmail.com>
2021-11-10 14:24:06 +0900

    Update code. Modify variables.

--- dataset_preprocess/myval.py
+++ dataset_preprocess/myval.py
@@ -1,8 +1,8 @@
-data_type = "train"
-file_type = ".json"
-input_path = "C:\\Users\\User\\Desktop\\annotation_test\\"
-output_path = "E:\\annotation_integrated\\"
-
+data_type = "train_additional" # select between train/test/val
+file_type = ".json" # file type .json
+input_path = "E:\\EVlegacy\\archive\\annotated_dataset\\" # dataset folder
+output_folder = "E:\\workspace\\EVCIDNet-main\\train_additional\\"
+researcher = "KTH" # People name
 json_dict = {"licenses": [{"name": "", "id": 0, "url": ""}],
              "info": {"contributor": "", "date_created": "", "description": "", "url": "", "version": "", "year": ""},
              "categories": [{"id": 1, "name": "HolePairLeft", "supercategory": ""},
