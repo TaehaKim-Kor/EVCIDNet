89fc46020e58c0cc99dd3d7abc62bc137ed9990f
TaehaKim <amvision1248@gmail.com>
2021-11-22 15:03:28 +0900

    초기 커밋.

---
+++ AzureKinectDK/main.cpp
@@ -0,0 +1,17 @@
+#include "myfunc.h"
+#include "mp_func.h"
+#include "mp_robot_func.h"
+int main()
+{
+	//char input[] = "C:\\Users\\anstn\\Desktop\\inputvideo\\output_2160p_2.mkv";
+	//char input[] = "output_2160p_1.mkv";
+	//Record2Image(input);
+	//MP_Sensor2Image();
+	MP_Sensor2Image2Robot();
+	//Color2Robot();
+	//Sensor2Image();
+	
+	//char test_input1[] = "C:\\User";
+	//char test_input2[] = "\\%03d.png";
+	//test_func(test_input1, test_input2);
+}
